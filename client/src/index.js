67ab7c9e0913372517d91b7b0e32028a633f5d1b
ajdeleon <ajdeleon1064@gmail.com>
2022-03-23 20:43:24 -0400

    add react alert for potential http error messaging

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
+const options = {
+  position: positions.TOP_CENTER,
+  timeout: 3000,
+  transition: transitions.SCALE
+}
 ReactDOM.render(
   <React.StrictMode>
+  <AlertProvider template={AlertTemplate} {...options}>
     <App />
+  </AlertProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
